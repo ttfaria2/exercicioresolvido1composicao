0fd636cc7458c06fc640892fb470a295d736649e
ttfaria2 <ttfaria2@gmail.com>
2020-08-06 16:31:28 -0300

    alterando a classe worker com a resposta do professor

--- src/main/java/entities/Worker.java
+++ src/main/java/entities/Worker.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Worker {
 
     private String name;
-    WorkerLevel level;
+    private WorkerLevel level;
     private Double baseSalary;
 
     private Department department;
@@ -16,7 +16,7 @@ public class Worker {
     public Worker() {
     }
 
-    public Worker(String name, WorkerLevel level, Double salary, Department department) {
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
         this.name = name;
         this.level = level;
         this.baseSalary = baseSalary;
@@ -27,36 +27,36 @@ public class Worker {
         return name;
     }
 
-    public WorkerLevel getLevel() {
-        return level;
-    }
-
-    public Double getBaseSalary() {
-        return baseSalary;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public List<HourContract> getContracts() {
-        return contracts;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
     }
 
     public void setLevel(WorkerLevel level) {
         this.level = level;
     }
 
-    public void setbaseSalary(Double baseSalary) {
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
         this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
     }
 
     public void setDepartment(Department department) {
         this.department = department;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
     }
 
     public void addContract(HourContract contract) {
@@ -67,18 +67,17 @@ public class Worker {
         contracts.remove(contract);
     }
 
-    public Double income(int year, int month) {
+    public double income(int year, int month) {
         double sum = baseSalary;
         Calendar cal = Calendar.getInstance();
-        for (HourContract c : contracts){
+        for (HourContract c : contracts) {
             cal.setTime(c.getDate());
             int c_year = cal.get(Calendar.YEAR);
-            int c_month = 1+ cal.get(Calendar.MONTH);
+            int c_month = 1 + cal.get(Calendar.MONTH);
             if (year == c_year && month == c_month) {
                 sum += c.totalValue();
             }
         }
         return sum;
     }
-
 }
